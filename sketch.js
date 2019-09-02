58de2abed354831c4528c7fdb0c54d1ec58c9118
David Kirchsteiger <e1402259@student.tuwien.ac.at>
2019-09-02 20:19:56 +0200

    Add game class

--- sketch.js
+++ sketch.js
@@ -1,30 +1,66 @@
-let player1;
-let player2;
-let playerSize = { width: 10, height: 70 };
-let ball;
+let backgroundGUI;
+let game;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
-  player1 = new Player({ x: 10, y: 10 }, playerSize);
-  player2 = new Player({ x: windowWidth - 10 - playerSize.width, y: 10 }, playerSize);
+  background(0);  // black background
 
-  ball = new Ball({x : windowWidth / 2, y: windowHeight / 2}, 20);
+  // drawing setup
+  stroke(255);  // white
+  rectMode(CENTER);
+
+  backgroundGUI = new Background();
+  backgroundGUI.draw();
+
+  game = new Game();
+  game.draw();
+
 }
 
-function draw() {  
-  player1.update();
-  player2.update();
+function draw() {
+  background(0);
+  backgroundGUI.draw();
 
-  player1.draw();
-  player2.draw();
-
-  ball.draw();
+  game.update();
+  game.draw();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   background(0);
+}
+
+class Game {
+  // physics
+  maxBallSpeed;
+
+  // stats
+  playerSize;
+
+  player1;
+  player2;
+  ball;
+
+  constructor() {
+    this.maxBallSpeed = 20;
+    this.playerSize = { width: 10, height: 70 };
+    this.player1 = new Player({ x: 10, y: windowHeight / 2 }, this.playerSize);
+    this.player2 = new Player({ x: windowWidth - 10, y: windowHeight / 2 }, this.playerSize);
+    this.ball = new Ball({ x: windowWidth / 2, y: windowHeight / 2 }, {x: 5, y: 1}, 20);
+  }
+
+  draw() {
+    this.player1.draw();
+    this.player2.draw();
+    this.ball.draw();
+  }
+
+  update() {
+    this.player1.update();
+    this.player2.update();
+    this.ball.update();
+  }
+
 }
 
 class Player {
@@ -40,25 +76,52 @@ class Player {
     rect(this.position.x, this.position.y, this.size.width, this.size.height);
   }
 
-  update(){
+  update() {
 
   }
 }
 
 class Ball {
   position = { x: undefined, y: undefined };
+  velocity = { x: undefined, y: undefined };
   radius;
 
-  constructor(startPosition, radius) {
+  constructor(startPosition, velocity, radius) {
     this.position = startPosition;
+    this.velocity = velocity;
     this.radius = radius;
+  }
+
+  setVelocity(velocity) {
+    this.velocity = velocity;
   }
 
   draw() {
     rect(this.position.x, this.position.y, this.radius, this.radius);
   }
 
-  update(){
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+class Background {
+
+  dashDist;
+
+  constructor() {
+    this.dashDist = 10;
+  }
+
+  draw() {
+    for (let i = 0; i < windowHeight; i += this.dashDist) {
+      line(windowWidth / 2, i, windowWidth / 2, i + this.dashDist / 2);
+    }
+  }
+
+  update() {
 
   }
+
 }
