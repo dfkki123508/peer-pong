abc9e5bbd81e0dba6a48661b6607c3fb64153ead
David Kirchsteiger <e1402259@student.tuwien.ac.at>
2020-01-29 20:26:48 +0100

    Window margin for sketch, util function to retrieve local IP for later usage

--- src/sketch.js
+++ src/sketch.js
@@ -4,9 +4,13 @@ export default function sketch(p) {
 
   let backgroundGUI;
   let game;
+  let margin = [20, 20];  // top/bottom, left/right
+  // TODO: set proper fullscreen
+  let width = window.innerWidth - margin[1];
+  let height = window.innerHeight - margin[0];
 
   p.setup = function () {
-    p.createCanvas(window.innerWidth, window.innerHeight);
+    p.createCanvas(width, height);
     p.background(0);  // black background
 
     // drawing setup
@@ -29,12 +33,13 @@ export default function sketch(p) {
   }
 
   p.windowResized = function () {
-    p.resizeCanvas(window.innerWidth, window.innerHeight);
+    p.resizeCanvas(width, height);
     p.background(0);
   }
 
   p.keyPressed = function () {
-    if (window.keyCode === 32) { // SPACE
+    // if (window.keyCode === 32) { // SPACE
+    if (p.keyIsDown(32)) {
       if (game) {
         game.startStop(); // toggle start/stop
         return false; // prevent default
@@ -71,9 +76,9 @@ export default function sketch(p) {
       this.maxBallSpeed = 15;
       this.borderWidth = 10;
       this.playerSize = { width: 10, height: 80 };
-      this.player1StartingPosition = p.createVector(10, window.innerHeight / 2);
-      this.player2StartingPosition = p.createVector(window.innerWidth - 10, window.innerHeight / 2);
-      this.playerMoveSpeed = 5 * window.innerHeight / 500;  // empirical value
+      this.player1StartingPosition = p.createVector(10, height / 2);
+      this.player2StartingPosition = p.createVector(width - 10, height / 2);
+      this.playerMoveSpeed = 5 * height / 500;  // empirical value
       this.score = [0, 0];
       this.resetRound();
     }
@@ -90,8 +95,8 @@ export default function sketch(p) {
       p.textAlign(p.CENTER, p.CENTER);
       p.fill(255);
       let gap = 40;
-      p.text(this.score[0], window.innerWidth / 2 - gap, 40);
-      p.text(this.score[1], window.innerWidth / 2 + gap, 40);
+      p.text(this.score[0], width / 2 - gap, 40);
+      p.text(this.score[1], width / 2 + gap, 40);
     }
 
     update() {
@@ -111,13 +116,13 @@ export default function sketch(p) {
     movePlayers() {
       if (p.keyIsDown(p.UP_ARROW) && this.player2.position.y - this.player2.size.height / 2 > this.borderWidth) {
         this.player2.position.y -= this.playerMoveSpeed;
-      } else if (p.keyIsDown(p.DOWN_ARROW) && this.player2.position.y + this.player2.size.height / 2 < window.innerHeight - this.borderWidth) {
+      } else if (p.keyIsDown(p.DOWN_ARROW) && this.player2.position.y + this.player2.size.height / 2 < height - this.borderWidth) {
         this.player2.position.y += this.playerMoveSpeed;
       }
 
       if (p.keyIsDown(87) && this.player1.position.y - this.player1.size.height / 2 > this.borderWidth) {
         this.player1.position.y -= this.playerMoveSpeed;
-      } else if (p.keyIsDown(83) && this.player1.position.y + this.player1.size.height / 2 < window.innerHeight - this.borderWidth) {
+      } else if (p.keyIsDown(83) && this.player1.position.y + this.player1.size.height / 2 < height - this.borderWidth) {
         this.player1.position.y += this.playerMoveSpeed;
       }
     }
@@ -136,12 +141,12 @@ export default function sketch(p) {
         this.ball.acceleration = reflectVector(this.N_LEFT, this.ball.acceleration);
       }
       // check if colliding with borders
-      else if (this.ball.position.x + this.ball.radius / 2 > window.innerWidth - this.borderWidth || this.ball.position.x - this.ball.radius / 2 < this.borderWidth) { // left/right, out of game
+      else if (this.ball.position.x + this.ball.radius / 2 > width - this.borderWidth || this.ball.position.x - this.ball.radius / 2 < this.borderWidth) { // left/right, out of game
         console.log('LEFT/RIGHT');
-        let scoreIdx = +!(this.ball.position.x + this.ball.radius / 2 > window.innerWidth - this.borderWidth)
+        let scoreIdx = +!(this.ball.position.x + this.ball.radius / 2 > width - this.borderWidth)
         this.score[scoreIdx]++;
         this.resetRound(false);
-      } else if (this.ball.position.y + this.ball.radius / 2 > window.innerHeight - this.borderWidth || this.ball.position.y - this.ball.radius / 2 < this.borderWidth) {  // top/bottom
+      } else if (this.ball.position.y + this.ball.radius / 2 > height - this.borderWidth || this.ball.position.y - this.ball.radius / 2 < this.borderWidth) {  // top/bottom
         console.log('TOP/BOTTOM');
         this.ball.velocity = reflectVector(this.N_DOWN, this.ball.velocity);
         this.ball.acceleration = reflectVector(this.N_DOWN, this.ball.acceleration);
@@ -155,7 +160,7 @@ export default function sketch(p) {
       this.player2 = new Player(this.player2StartingPosition.copy(), this.playerSize);
       let ballStartingVelocity = p.createVector(rand(-5, 5), rand(-2.5, 2.5));
       let ballStartingAccelartion = ballStartingVelocity.copy().mult(rand(0.001, 0.01));  // Has to go in the same direction
-      this.ball = new Ball(p.createVector(window.innerWidth / 2, window.innerHeight / 2), ballStartingVelocity, ballStartingAccelartion, 20, this.maxBallSpeed);
+      this.ball = new Ball(p.createVector(width / 2, height / 2), ballStartingVelocity, ballStartingAccelartion, 20, this.maxBallSpeed);
     }
   }
 
@@ -211,8 +216,8 @@ export default function sketch(p) {
     }
 
     draw() {
-      for (let i = 0; i < window.innerHeight; i += this.dashDist) {
-        p.line(window.innerWidth / 2, i, window.innerWidth / 2, i + this.dashDist / 2);
+      for (let i = 0; i < height; i += this.dashDist) {
+        p.line(width / 2, i, width / 2, i + this.dashDist / 2);
       }
     }
 
