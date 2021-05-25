5703885b818ba8d369bc535fdab15fa80f69bfb8
David Kirchsteiger <e1402259@student.tuwien.ac.at>
2021-05-25 14:11:24 +0200

    prohibit remote startgame when not in pause state

--- src/util/MessageHandler/StartGameMessageHandler.ts
+++ src/util/MessageHandler/StartGameMessageHandler.ts
@@ -1,6 +1,11 @@
 import Game from '../../controllers/Game';
+import { gameState$ } from '../../services/GameStore';
 import P2PService from '../../services/P2PService';
-import { MESSAGE_EVENTS, StartGameMessageDataType } from '../../types/types';
+import {
+  GAME_STATE,
+  MESSAGE_EVENTS,
+  StartGameMessageDataType,
+} from '../../types/types';
 import AbstractMessageHandler from './AbstractMessageHandler';
 
 class StartGameMessageHandler extends AbstractMessageHandler<StartGameMessageDataType> {
@@ -16,7 +21,7 @@ class StartGameMessageHandler extends AbstractMessageHandler<StartGameMessageDat
     console.log('Start game received', this.data);
     const p2pService = P2PService.getInstance();
     const game = Game.getInstance();
-    if (game) {
+    if (game && gameState$.getValue() === GAME_STATE.pause) {
       // TODO: use p2pService latency here, in case browser times are out of sync
       const delay = Date.now() - this.timestampCreated;
       console.log('with Delay', delay, 'avg. latency', p2pService.avgLatency);
